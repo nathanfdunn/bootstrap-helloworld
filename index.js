a021f62c1a84b7349417059e5cfdb353c03e2531
Nathan Dunn <nathanfdunn@gmail.com>
2016-02-07 15:26:50 -0500

    Modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
-var port = 5000
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function(){
-	console.log('Bootstrap Hello World running on port', port);
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
